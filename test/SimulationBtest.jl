c9827f7a55147d964a8879a55cfa6e567719b2a3
juliansaltducaju <48758475+juliansaltducaju@users.noreply.github.com>
2019-08-21 13:59:19 +0200

    Update SimulationBtest.jl

--- test/SimulationBtest.jl
+++ test/SimulationBtest.jl
@@ -1,10 +1,25 @@
-using Test, JuliansMPCforJulia
+using Test, PandaMPC
 
 using ECOS
 using Convex
 using Plots
 
-import JuliansMPCforJulia: weights, model, go, iteraciones2, plotjoint
+import PandaMPC
+
+#Initial conditions
+x0 = -1;
+y0 = 0;
+z0 = 0;
+v0x = 0.9;
+v0y = 0.2;
+v0z = 4;
+cond0 = [x0; y0; z0; v0x; v0y; v0z];
+simtimeball = 1; # Simulation time
+
+# Here we get the trajectory of the ball
+t, posdrag, posnodrag = ball(cond0, simtimeball)
+xplane = 0.4; #distance of grabbing to the ball from the base
+xcapture, ycapture, zcapture, tcapture = intercplane(t, posdrag, xplane)
 
 pesoQ = 1 #state penalization weight
 pesoR = 0.001 #input penalization weight
@@ -15,3 +30,4 @@ m = 1 #number of joints
 Q = [0 0 0; 0 1 0; 0 0 1]
 R = pesoR
 @test (Q,R) == weights(pesoQ, pesoR, m)
+@test (xcapture, ycapture, zcapture, tcapture) == intercplane(t, posdrag, xplane)
